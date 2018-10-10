1a2675aa9c45c00c949e9dc2db081c8b867d0aae
ancamora <ancamondragonra@ittepic.edu.mx>
2018-10-10 15:25:22 -0600

    t03

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { OtherPage } from '../otherPage/otherPage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
-})
-export class HomePage {
-
+ })
+ export class HomePage {
+    public myString: string = 'ejemplo 1 STRING';
+    public myJSON: any = {text: ''};
+    otherPage: any = OtherPage;
   constructor(public navCtrl: NavController) {
-
   }
-
-}
+    gotoOtherPage() {
+    this.navCtrl.push(OtherPage, {text: 'ejemplo 3 OBJETO'});
+    }
+  }
